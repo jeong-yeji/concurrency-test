252ac0f3a955143a4bedff3a006621c78bc9970f
jeong-yeji <jeongyeji.mail@gmail.com>
2023-11-22 15:42:01 +0900

    Redis Redisson을 이용한 문제 해결

---
+++ src/test/java/com/example/stock/facade/RedissonLockStockFacadeTest.java
@@ -0,0 +1,58 @@
+package com.example.stock.facade;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedissonLockStockFacadeTest {
+
+    @Autowired
+    private RedissonLockStockFacade redissonLockStockFacade;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    void setUp() {
+        stockRepository.saveAndFlush(new Stock(1L, 100L));
+    }
+
+    @AfterEach
+    void tearDown() {
+        stockRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("동시에 100개의 재고 감소 요청을 보내는 경우")
+    @Test
+    void decrease_stock_quantity_100() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    redissonLockStockFacade.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertEquals(0, stock.getQuantity());
+    }
+}
